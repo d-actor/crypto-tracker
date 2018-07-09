a5a70faedcb35fd3e0ac979a617fcdad380400ef
Dan Actor <danltactor@gmail.com>
2018-07-08 22:11:54 -0600

    git init

---
+++ client/src/reducers/coins.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+const COINS = 'COINS';
+const ADD_COIN = 'ADD_COIN';
+const REMOVE_COIN = 'REMOVE_COIN';
+
+export const addCoin = (coin) => {
+  return (dispatch) => {
+    axios.post('/api/coins', { coin })
+      .then( ({ data, headers }) => dispatch({ type: ADD_COIN, coin: data, headers }) )
+  }
+}
+
+export const getCoins = () => {
+  return (dispatch) => {
+    axios.get('/api/coins')
+      .then( ({ data, headers }) => dispatch({ type: COINS, coins: data, headers }) )
+  }
+}
+
+export const removeCoin = (id) => {
+  return (dispatch) => {
+    axios.put(`/api/coins/${id}`)
+      .then( ({ headers }) => dispatch({ type: REMOVE_COIN, id, headers }) )
+  }
+}
+
+const coins = (state = [], action) => {
+  switch (action.type) {
+    case COINS:
+      return action.coins;
+    case ADD_COIN:
+      return [...state, action.coin]
+    case REMOVE_COIN:
+      return state.filter( c => c.id !== action.id )
+    default:
+      return state
+  }
+}
+
+export default coins;
+
